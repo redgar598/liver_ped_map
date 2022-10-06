c9c3760521f5875920b3c42e92a3e53c06d55e06
redgar598 <redgar598@gmail.com>
2022-10-06 17:00:45 -0400

    more memory

--- targets_run.sh
+++ targets_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 1:00 
-#SBATCH --mem=256M
+#SBATCH --mem=5G
 #SBATCH -J test 
 #SBATCH -p all 
 #SBATCH -c 1 
@@ -13,3 +13,4 @@ module load R/4.2.1
 
 
 Rscript R_functions/test.R
+
