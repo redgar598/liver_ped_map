11815d6cc43f273ad7d405f7cbf08ee8bffbc62d
redgar598 <redgar598@gmail.com>
2022-10-07 09:54:46 -0400

    try with targets

--- R_functions/functions.R
+++ R_functions/functions.R
@@ -9,14 +9,16 @@ load_d10x_raw <- function(dataset_loc){
   #dataset_loc <- here("../../../projects/macparland/RE/PediatricAdult")
   
   samples<-list.files(dataset_loc)
+  print(samples)
   
-  d10x.data <- sapply(samples, function(y){
+  d10x.data <- sapply(1:length(samples), function(y){
+    print(file.path(dataset_loc,paste(samples[y], sep=""),"filtered_feature_bc_matrix"))
     d10x <- Read10X(file.path(dataset_loc,paste(samples[y], sep=""),"filtered_feature_bc_matrix"))
-    colnames(d10x) <- paste(sapply(strsplit(colnames(d10x),split="-"),'[[',1L),i,sep="-")
+    #colnames(d10x) <- paste(sapply(strsplit(colnames(d10x),split="-"),'[[',1L),i,sep="-")
     d10x
   })
   d10x.data<-do.call("cbind",d10x.data)
-    
+  
   #' Initialize the Seurat object with the raw (non-normalized data).
   d10x <- CreateSeuratObject(counts = d10x.data, project = "ped_adult_map", min.cells = 3, min.features = 0)
   d10x
--- targets_run.sh
+++ targets_run.sh
@@ -12,6 +12,6 @@ module load R/4.2.1
 
 
 
-##Rscript R_functions/test.R
+Rscript R_functions/test.R
 
-Rscript R_functions/test_notarget.R
+##Rscript R_functions/test_notarget.R
