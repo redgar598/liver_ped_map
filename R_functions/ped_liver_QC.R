83340d828eaa5ed247c2008eadc03ecfe8607ce6
redgar598 <redgar598@gmail.com>
2022-10-13 11:59:59 -0400

    maye now

--- R_functions/ped_liver_QC.R
+++ R_functions/ped_liver_QC.R
@@ -15,6 +15,7 @@ samples<-samples[-grep("meta",samples)]
 print(samples)
 
 meta<-read.table(here(dataset_loc,"input_metadata.txt"), header=T)
+meta$Sample_ID[which(meta$Sample_ID=="C85_caud3pr")]<-"C85_caud5pr"
 #meta<-read.table(here("data/input_metadata.txt"), header=T)
 
 
@@ -32,10 +33,7 @@ d10x.list <- sapply(1:length(samples), function(y){
   meta_cell_add<-merge(meta_cell, meta, by.x="individual", by.y="Sample_ID")
   meta_cell_add<-meta_cell_add[match(colnames(d10x), meta_cell_add$cell),]
   # print(identical(meta_cell_add$cell, colnames(d10x)))
-  print(head(meta_cell_add))
-  print(head(d10x@meta.data))
   rownames(meta_cell_add)<-meta_cell_add$cell
-  
   d10x<- AddMetaData(d10x, meta_cell_add)
   d10x
 })
