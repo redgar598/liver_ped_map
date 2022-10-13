c099c7f1b7a1f429c5f0d0b41585eca8bd90f4d3
redgar598 <redgar598@gmail.com>
2022-10-13 09:54:36 -0400

    try more samples

--- R_functions/test_notarget.R
+++ R_functions/test_notarget.R
@@ -7,16 +7,13 @@ dataset_loc <- here("../../../projects/macparland/RE/PediatricAdult")
 samples<-list.files(dataset_loc)
 print(samples)
 
-d10x.data <- sapply(1:length(samples), function(y){
+d10x.list <- sapply(1:length(samples), function(y){
   print(file.path(dataset_loc,paste(samples[y], sep=""),"filtered_feature_bc_matrix"))
   d10x <- Read10X(file.path(dataset_loc,paste(samples[y], sep=""),"filtered_feature_bc_matrix"))
   colnames(d10x) <- paste(sapply(strsplit(colnames(d10x),split="-"),'[[',1L),samples[y],sep="-")
   print(dim(d10x))
-  d10x
+  #' Initialize the Seurat object with the raw (non-normalized data).
+  CreateSeuratObject(counts = d10x.data, project = "ped_adult_map", min.cells = 3, min.features = 0)
 })
-d10x.data<-do.call("cbind",d10x.data)
 
-#' Initialize the Seurat object with the raw (non-normalized data).
-d10x <- CreateSeuratObject(counts = d10x.data, project = "ped_adult_map", min.cells = 3, min.features = 0)
-d10x
-
+d10x.list
