00ecf73cafc2892f5f3e1a3b3b01909815a8fdc2
redgar598 <redgar598@gmail.com>
2022-11-07 12:50:51 -0500

    again

--- R_functions/ped_liver_QC.R
+++ R_functions/ped_liver_QC.R
@@ -62,10 +62,10 @@ d10x.list <- sapply(1:length(samples), function(y){
   
   #add meta data to each seurat object
   meta_cell<-data.frame(cell=colnames(d10x), individual=sapply(colnames(d10x), function(x) strsplit(x,"-")[[1]][2]))
-  # print(head(meta_cell))
+  print(head(meta_cell))
   meta_cell_add<-merge(meta_cell, meta, by.x="individual", by.y="Sample_ID")
   meta_cell_add<-meta_cell_add[match(colnames(d10x), meta_cell_add$cell),]
-  # print(identical(meta_cell_add$cell, colnames(d10x)))
+  print(identical(meta_cell_add$cell, colnames(d10x)))
   rownames(meta_cell_add)<-meta_cell_add$cell
   d10x<- AddMetaData(d10x, meta_cell_add)
   d10x
