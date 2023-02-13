3e5f696d75acd95c95bb971d2fa708adc5db0607
redgar598 <redgar598@gmail.com>
2023-02-13 15:02:44 -0500

    soup X test

--- test_run.sh
+++ test_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --mem=5G
-#SBATCH -J droplet_QC_test
-#SBATCH -p all 
+#SBATCH --mem=100G
+#SBATCH -J soup_X
+#SBATCH -p veryhimem
 #SBATCH -c 32 
 #SBATCH -N 1 
 #SBATCH --output=%x.out
@@ -9,5 +9,5 @@
 
 module load R/4.2.1
 
-Rscript scripts/test.R
+Rscript scripts/soupX_explore.R
 
