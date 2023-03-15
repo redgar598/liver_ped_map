4ef0dd4a8e13c6859185bb6108e5606bfe988974
redgar598 <redgar598@gmail.com>
2023-03-15 09:34:55 -0400

    ready for 39

--- scripts/01_ped_liver_QC_with_dropletQC.R
+++ scripts/01_ped_liver_QC_with_dropletQC.R
@@ -26,7 +26,7 @@ samples<-samples[-grep("data_transfer",samples)]
 print(samples)
 
 #meta<-read.table(here("data/data_transfer_updated_mar14_2023.csv"), header=T, sep=",")
-meta<-read.table(here(dataset_loc,"data_transfer_updated_mar14_2023_no39.csv"), header=T, sep=",")
+meta<-read.table(here(dataset_loc,"data_transfer_updated_mar14_2023.csv"), header=T, sep=",")
 
 d10x.list <- sapply(1:length(samples), function(y){
   caud<-meta$Sample_ID[which(meta$file == samples[y])]
@@ -37,52 +37,63 @@ d10x.list <- sapply(1:length(samples), function(y){
   # print(dim(d10x))
   #' Initialize the Seurat object with the raw (non-normalized data).
   d10x<-CreateSeuratObject(counts = d10x, project = "ped_adult_map", min.cells = 0, min.features = 0)
-
-  ## SoupX needs clusters so quickly make clusters for each sample
-  d10x    <- SCTransform(d10x, verbose = F)
-  d10x    <- RunPCA(d10x, verbose = F)
-  d10x    <- RunUMAP(d10x, dims = 1:30, verbose = F)
-  d10x    <- FindNeighbors(d10x, dims = 1:30, verbose = F)
-  d10x    <- FindClusters(d10x, verbose = T)
-  meta_clusters    <- d10x@meta.data
-
-  sc = load10X(file.path(dataset_loc,paste(samples[y],"/outs", sep="")))
-  sc = setClusters(sc, setNames(meta_clusters$seurat_clusters, rownames(meta_clusters)))
-
-  ######
-  ## Load data and estimate soup profile
-  ######
-  # Estimate rho
-  sc = autoEstCont(sc, forceAccept=TRUE)
-  #Genes with highest expression in background. These are often enriched for ribosomal proteins.
-  print(head(sc$soupProfile[order(sc$soupProfile$est, decreasing = T), ], n = 20))
-  print(unique(sc$metaData$rho))
-  # Clean the data
-  out = adjustCounts(sc)
   
-  ## Save a metric of soupness (ALB change after soupX)
-  DR = sc$metaData[,sc$DR]
-  df = DR
-  old = colSums(sc$toc["ALB",rownames(df),drop=FALSE])
-  new = colSums(out["ALB",rownames(df),drop=FALSE])
-  relChange = (old-new)/old
-  df$old = old
-  df$new = new
-  df$relALBChange=relChange
-  df$cell<-rownames(df)
-
-  ## make seurat object of adjusted counts
-  d10x = CreateSeuratObject(out)
+  if(dir.exists(file.path(dataset_loc,paste(samples[y],"/outs/raw_feature_bc_matrix", sep="")))) {
+    ## SoupX needs clusters so quickly make clusters for each sample
+    d10x    <- SCTransform(d10x, verbose = F)
+    d10x    <- RunPCA(d10x, verbose = F)
+    d10x    <- RunUMAP(d10x, dims = 1:30, verbose = F)
+    d10x    <- FindNeighbors(d10x, dims = 1:30, verbose = F)
+    d10x    <- FindClusters(d10x, verbose = T)
+    meta_clusters    <- d10x@meta.data
+    
+    sc = load10X(file.path(dataset_loc,paste(samples[y],"/outs", sep="")))
+    sc = setClusters(sc, setNames(meta_clusters$seurat_clusters, rownames(meta_clusters)))
+    
+    ######
+    ## Load data and estimate soup profile
+    ######
+    # Estimate rho
+    sc = autoEstCont(sc, forceAccept=TRUE)
+    #Genes with highest expression in background. These are often enriched for ribosomal proteins.
+    print(head(sc$soupProfile[order(sc$soupProfile$est, decreasing = T), ], n = 20))
+    print(unique(sc$metaData$rho))
+    # Clean the data
+    out = adjustCounts(sc)
+    
+    ## Save a metric of soupness (ALB change after soupX)
+    DR = sc$metaData[,sc$DR]
+    df = DR
+    old = colSums(sc$toc["ALB",rownames(df),drop=FALSE])
+    new = colSums(out["ALB",rownames(df),drop=FALSE])
+    relChange = (old-new)/old
+    df$old = old
+    df$new = new
+    df$relALBChange=relChange
+    df$cell<-rownames(df)
+    
+    ## make seurat object of adjusted counts
+    d10x = CreateSeuratObject(out)
+    
+    #add meta data to each seurat object
+    meta_cell<-data.frame(cell=colnames(d10x), individual=caud)
+    meta_cell_add<-merge(meta_cell, meta, by.x="individual", by.y="Sample_ID")
+    meta_cell_add<-merge(meta_cell_add, df[,c("cell","relALBChange")], by="cell")
+    meta_cell_add<-meta_cell_add[match(colnames(d10x), meta_cell_add$cell),]
+    print(identical(meta_cell_add$cell, colnames(d10x)))
+    rownames(meta_cell_add)<-meta_cell_add$cell
+    d10x<- AddMetaData(d10x, meta_cell_add)
+  }else{
+    #add meta data to each seurat object
+    meta_cell<-data.frame(cell=colnames(d10x), individual=caud)
+    meta_cell_add<-merge(meta_cell, meta, by.x="individual", by.y="Sample_ID")
+    meta_cell_add$relALBChange<-NA
+    meta_cell_add<-meta_cell_add[match(colnames(d10x), meta_cell_add$cell),]
+    print(identical(meta_cell_add$cell, colnames(d10x)))
+    rownames(meta_cell_add)<-meta_cell_add$cell
+    d10x<- AddMetaData(d10x, meta_cell_add)
+  }
   
-  #add meta data to each seurat object
-  meta_cell<-data.frame(cell=colnames(d10x), individual=caud)
-  meta_cell_add<-merge(meta_cell, meta, by.x="individual", by.y="Sample_ID")
-  meta_cell_add<-merge(meta_cell_add, df[,c("cell","relALBChange")], by="cell")
-  meta_cell_add<-meta_cell_add[match(colnames(d10x), meta_cell_add$cell),]
-  print(identical(meta_cell_add$cell, colnames(d10x)))
-  rownames(meta_cell_add)<-meta_cell_add$cell
-  d10x<- AddMetaData(d10x, meta_cell_add)
-
   ######
   ## dropletQC
   ######
@@ -91,22 +102,22 @@ d10x.list <- sapply(1:length(samples), function(y){
       outs = file.path(dataset_loc,paste(samples[y],"/outs", sep="")),
       tiles = 1, cores = 1, verbose = FALSE)
     head(nf1)
-
+    
     print(identical(rownames(nf1), colnames(d10x)))
     d10x<- AddMetaData(d10x, nf1)
     d10x
-
+    
     nf.umi <- data.frame(nf=d10x$nuclear_fraction,
-                             umi=d10x$nCount_RNA)
-
+                         umi=d10x$nCount_RNA)
+    
     # Run identify_empty_drops
     empty_drop <- identify_empty_drops(nf_umi=nf.umi)
     empty_drop$individual<-d10x$individual
     empty_drop_damagedcell <- identify_damaged_cells(empty_drop, verbose = FALSE, output_plots = F)
-
+    
     head(empty_drop_damagedcell[[1]])
     table(empty_drop_damagedcell[[1]]$cell_status)
-
+    
     print(identical(rownames(empty_drop_damagedcell[[1]]), colnames(d10x)))
     d10x<- AddMetaData(d10x, empty_drop_damagedcell[[1]])
     d10x$nf<-NULL
@@ -660,7 +671,7 @@ d10x.combined@meta.data$CellType_rough[which(d10x.combined@meta.data$seurat_clus
 ## Individual plots
 ##########
 d10x.combined$age_id<-paste(d10x.combined$individual, d10x.combined$AgeGroup)
-          #d10x.combined$age_id[which(d10x.combined$age_id=="C85_caud5pr Ped")]<-"C85_caud5pr Ped (Frozen)"
+#d10x.combined$age_id[which(d10x.combined$age_id=="C85_caud5pr Ped")]<-"C85_caud5pr Ped (Frozen)"
 d10x.combined$age_id<-as.factor(d10x.combined$age_id)
 #order by % cell passing QC
 #' d10x.combined$age_id<-factor(d10x.combined$age_id, c("C86_caud3pr Adult","C92_caud3pr Adult", "C63_caud5pr Adult","C70_caud5pr Adult",
