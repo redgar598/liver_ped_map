93d5450ec91b89a610d5c3e6e8ea733eab7916c7
redgar598 <redgar598@gmail.com>
2023-03-16 13:35:40 -0400

    fetal liver seurat

--- scripts/fetal_liver.R
+++ scripts/fetal_liver.R
@@ -3,10 +3,12 @@ library(Seurat)
 library(here)
 
 #https://developmental.cellatlas.io/fetal-liver
-fetal_liver_h5ad <- Convert(here("/media/redgar/Seagate Portable Drive/fetal_liver/download.h5ad"), ".h5seurat", overwrite= TRUE)
+#fetal_liver_h5ad <- Convert(here("/media/redgar/Seagate Portable Drive/fetal_liver/download.h5ad"), ".h5seurat", overwrite= TRUE)
+fetal_liver_h5ad <- Convert(here("../../../projects/macparland/RE/PediatricAdult/fetal_liver/download.h5ad"), ".h5seurat", overwrite= TRUE)
+
 fetal_liver <- LoadH5Seurat(fetal_liver_h5ad, assays = "RNA")
 
 print(dim(fetal_liver))
 print(fetal_liver)
 
-#saveRDS(here("data/adult_intestine","local.rds"))
+saveRDS(fetal_liver, here("../../../projects/macparland/RE/PediatricAdult/fetal_liver","fetal_liver.rds"))
