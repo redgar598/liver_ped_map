b7720e05f0c6f08fde2d7842a16fc916b1336150
redgar598 <redgar598@gmail.com>
2023-06-21 12:48:02 -0400

    pipeline organization

---
+++ ped_liver_IFALD_pipeline.sh
@@ -0,0 +1,16 @@
+01_ped_liver_QC_with_dropletQC_SCINA_IFALD.R
+02_IFALD_ped_adult_fetal_integration.R
+cellxgene_ped_adult_ifald.R
+
+03_PBMC_correction_IFALD
+03_IFALD_Tcells.R
+03_IFALD_myeloid.R
+03_IFALD_HSC.R
+03_IFALD_Bcells.R
+
+04_differential_expression_montecarlo.R
+04_differential_expression_montecarlo_withFetal_IFALD.R
+
+05_differential_expression_age_interpretation.R
+
+06_cell_composistion.R
