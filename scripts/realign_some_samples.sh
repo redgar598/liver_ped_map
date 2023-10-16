8f4fd92b54da08817bd0e0ce0a586578dbb08c2a
redgar598 <redgar598@gmail.com>
2023-10-16 14:29:39 -0400

    now try cell ranger

--- scripts/realign_some_samples.sh
+++ scripts/realign_some_samples.sh
@@ -9,13 +9,13 @@
 
 module load cellranger/3.1.0
 
-cellranger bamtofastq --nthreads=8 /cluster/projects/macparland/RE/PediatricAdult/realign_samples/possorted_genome_bam.bam  /cluster/projects/macparland/RE/PediatricAdult/realign_samples/IFALD006
+#cellranger bamtofastq --nthreads=8 /cluster/projects/macparland/RE/PediatricAdult/realign_samples/possorted_genome_bam.bam  /cluster/projects/macparland/RE/PediatricAdult/realign_samples/IFALD006
 
 
-#cellranger count --id=IFALD006_realign \
-#   --fastqs=/cluster/projects/macparland/RE/PediatricAdult/realign_samples/... \
-#   --sample=IFALD006_realign \
-#   --transcriptome=/cluster/tools/software/centos7/cellranger/3.1.0/cellranger-tiny-ref/3.0.0 
+cellranger count --id=IFALD006_realign \
+   --fastqs=/cluster/projects/macparland/RE/PediatricAdult/realign_samples/IFALD006/MacParland_Sonya__HSC-FI_006_0_1_HMW73DMXX \
+   --sample=IFALD006_realign \
+   --transcriptome=/cluster/tools/software/centos7/cellranger/3.1.0/cellranger-tiny-ref/3.0.0 
 
 
 
