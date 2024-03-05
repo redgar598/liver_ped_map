16b3f128eda3cb057fcafe8f7a404168edc8f2bf
redgar598 <redgar598@gmail.com>
2024-03-05 15:09:15 -0500

    now fetal

--- ped_liver_IFALD_pipeline.sh
+++ ped_liver_IFALD_pipeline.sh
@@ -1,27 +1,37 @@
 01_ped_liver_QC_with_dropletQC_SCINA_IFALD.R
+01_fetal_liver.R
+
 02_ped_liver_QC_with_dropletQC_SCINA_IFALD_PBMC_only.R 
 
 03_IFALD_myeloid.R
 03_IFALD_Bcells.R 
 IFALD_presentation_plots.R
 04_PBMC_correction_IFALD 
+03_IFALD_HSC.R
 
 
 
 ## running
-03_IFALD_HSC.R
+cellphonedb_datasetup.R
+cellphonedb_interpretation.R
+
 
 
 ## to do 
 02_IFALD_ped_adult_fetal_integration.R
 04_Fetal_PCA_roughwork
 cellxgene_ped_adult_ifald.R
+06_adolescent.R
 
 cpdb_age
 
 	cpdb_IFALD
 
 	#03_IFALD_Tcells.R
+	#03 colangiocyted
+	#03 LSEC
+	#03 hepatocytes
+	#
 
 	#04_differential_expression_montecarlo.R
 	#04_differential_expression_montecarlo_withFetal_IFALD.R
