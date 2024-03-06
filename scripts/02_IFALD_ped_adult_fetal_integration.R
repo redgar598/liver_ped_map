32f752768c3ea9d15ae704d9b56095b808c31125
redgar598 <redgar598@gmail.com>
2024-03-06 10:42:11 -0500

    split integrated

--- scripts/02_IFALD_ped_adult_fetal_integration.R
+++ scripts/02_IFALD_ped_adult_fetal_integration.R
@@ -17,139 +17,141 @@ library(RColorBrewer)
 source("scripts/00_pretty_plots.R")
 source("scripts/00_entropy_d10x.R")
 
-#################
-## Load raw QC'ed data
-#################
-d10x_fetal<-readRDS(here("../../../projects/macparland/RE/PediatricAdult/processed_data","d10x_fetal_raw.rds"))
-#d10x_fetal<-readRDS("/media/redgar/Seagate Portable Drive/fetal_liver/d10x_fetal_raw.rds")
+# #################
+# ## Load raw QC'ed data
+# #################
+# d10x_fetal<-readRDS(here("../../../projects/macparland/RE/PediatricAdult/processed_data","d10x_fetal_raw.rds"))
+# #d10x_fetal<-readRDS("/media/redgar/Seagate Portable Drive/fetal_liver/d10x_fetal_raw.rds")
+# 
+# d10x_ped_IFALD<-readRDS(file = here("../../../projects/macparland/RE/PediatricAdult/processed_data","IFALD_d10x_adult_ped_raw.rds"))
+# ## add cell type labels
+# load(here("../../../projects/macparland/RE/PediatricAdult/processed_data","IFALD_adult_ped_cellRefined_withDropletQC.rds"))
+# cell_label$index<-rownames(cell_label)
+# cell_label<-cell_label[match(colnames(d10x_ped_IFALD), cell_label$index),]
+# identical(colnames(d10x_ped_IFALD), cell_label$index)
+# d10x_ped_IFALD <- AddMetaData(d10x_ped_IFALD, metadata = cell_label)
+# 
+# 
+# #################
+# ## Match meta data
+# #################
+# d10x_fetal@meta.data$Barcodes<-NULL
+# colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Extract.Name")]<-"Sample"
+# colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Characteristics.individual.")]<-"individual"
+# d10x_fetal@meta.data$Treatment<-"Healthy"
+# d10x_fetal@meta.data$Tissue<-"TLH"
+# d10x_fetal@meta.data$chemistry<-"3pr"
+# colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Characteristics.sex.")]<-"Sex"
+# colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Characteristics.age.")]<-"Age"
+# d10x_fetal@meta.data$AgeGroup<-"Fetal"
+# d10x_fetal@meta.data$FreshorFrozen<-"fresh"
+# d10x_fetal@meta.data$BMI<-NA
+# d10x_fetal@meta.data$relALBChange<-NA
+# d10x_fetal@meta.data$nuclear_fraction<-NA
+# d10x_fetal@meta.data$cell_status<-NA
+# d10x_fetal@meta.data$Perfused<-NA
+# colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Cell.Labels")]<-"CellType_refined"
+# d10x_fetal@meta.data$age_condition<-paste(d10x_fetal$AgeGroup, d10x_fetal$Treatment, sep=" ")
+# 
+# 
+# d10x_ped_IFALD@meta.data$file<-NULL
+# d10x_ped_IFALD@meta.data$Approx_bam_GB<-NULL
+# d10x_ped_IFALD@meta.data$Characteristics.facs.sorting.<-NA
+# d10x_ped_IFALD@meta.data$Sample<-d10x_ped_IFALD@meta.data$individual
+# d10x_ped_IFALD@meta.data$individual<-sapply(1:nrow(d10x_ped_IFALD@meta.data), function(x) strsplit(d10x_ped_IFALD@meta.data$individual[x],"_")[[1]][1])
+# d10x_ped_IFALD@meta.data$CellType_rough <-NULL
+# d10x_ped_IFALD@meta.data$second_best_cell  <-NULL
+# d10x_ped_IFALD@meta.data$S.Score   <-NULL
+# d10x_ped_IFALD@meta.data$G2M.Score <-NULL
+# d10x_ped_IFALD@meta.data$Phase     <-NULL
+# d10x_ped_IFALD@meta.data$old.ident<-NULL
+# d10x_ped_IFALD@meta.data$integrated_snn_res.0.5 <-NULL
+# d10x_ped_IFALD@meta.data$seurat_clusters<-NULL
+# d10x_ped_IFALD@meta.data$age_id<-NULL
+# d10x_ped_IFALD@meta.data$index<-NULL
+# d10x_ped_IFALD@meta.data$SCINA_broad<-NULL
+# d10x_ped_IFALD@meta.data$SCINA_refined<-NULL
+# d10x_ped_IFALD@meta.data$cluster_consensus<-NULL
+# d10x_ped_IFALD@meta.data$Characteristics.clinical.information.<-NA
+# 
+# 
+# head(d10x_ped_IFALD@meta.data)
+# head(d10x_fetal@meta.data)
+# 
+# d10x_fetal@meta.data<-d10x_fetal@meta.data[,colnames(d10x_ped_IFALD@meta.data)]
+# 
+# head(d10x_fetal@meta.data)
+# head(d10x_ped_IFALD@meta.data)
+# 
+# 
+# 
+# 
+# 
+# 
+# #################
+# ## Merge
+# #################
+# d10x <- merge(d10x_ped_IFALD,d10x_fetal, merge.data=TRUE, project = "IFALD_fetal_adult_ped_map")
+# saveRDS(d10x, file = here("../../../projects/macparland/RE/PediatricAdult/processed_data","Fetal_IFALD_d10x_adult_ped_raw.rds"))
+# 
+# ###############
+# ## Integrate by donor
+# ###############
+# #https://satijalab.org/seurat/articles/integration_rpca.html
+# print("RUNNING INTEGRATION")
+# 
+# ## run integration across donor and hopefully that will also smooth out differences with chemistry and batch?
+# d10x.list<- SplitObject(d10x, split.by = "Sample")
+# 
+# # normalize, identify variable features and score cell cycle for each dataset independently
+# s.genes <- cc.genes$s.genes
+# g2m.genes <- cc.genes$g2m.genes
+# 
+# d10x.list <- lapply(X = d10x.list, FUN = function(x) {
+#   x <- NormalizeData(x)
+#   x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
+#   x <- CellCycleScoring(x, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+# })
+# 
+# # select features that are repeatedly variable across datasets for integration run PCA on each
+# # dataset using these features
+# features <- SelectIntegrationFeatures(object.list = d10x.list)
+# d10x.list <- lapply(X = d10x.list, FUN = function(x) {
+#   #x <- ScaleData(x, features = features, verbose = FALSE)
+#   x <- ScaleData(x, vars.to.regress = c("nFeature_RNA","S.Score", "G2M.Score"), features = features, verbose = FALSE)
+#   x <- RunPCA(x, features = features, verbose = FALSE)
+# })
+# 
+# ## Identify anchors
+# chem.anchors <- FindIntegrationAnchors(object.list = d10x.list, anchor.features = features, reduction = "rpca")
+# d10x.fetal_ped_IFALD <- IntegrateData(anchorset = chem.anchors)
+# 
+# DefaultAssay(d10x.fetal_ped_IFALD) <- "integrated"
+# 
+# print("INTEGRATED")
+# 
+# 
+# # Run the standard workflow for visualization and clustering
+# d10x.fetal_ped_IFALD <- ScaleData(d10x.fetal_ped_IFALD, verbose = FALSE)
+# d10x.fetal_ped_IFALD <- RunPCA(d10x.fetal_ped_IFALD, npcs = 30, verbose = FALSE)
+# d10x.fetal_ped_IFALD <- RunUMAP(d10x.fetal_ped_IFALD, reduction = "pca", dims = 1:30)
+# d10x.fetal_ped_IFALD <- RunTSNE(d10x.fetal_ped_IFALD, dims = 1:30)
+# 
+# d10x.fetal_ped_IFALD <- FindNeighbors(d10x.fetal_ped_IFALD, reduction = "pca", dims = 1:30)
+# d10x.fetal_ped_IFALD <- FindClusters(d10x.fetal_ped_IFALD, resolution = 0.5)
+# 
+# d10x.fetal_ped_IFALD
+# 
+# 
+# ##############
+# ## Save integrated to look local
+# ##############
+# save(d10x.fetal_ped_IFALD, file=paste(here("../../../projects/macparland/RE/PediatricAdult/processed_data"),"Fetal_IFALD_adult_ped_integrated.rds", sep=""))
+# cell_label<-d10x.fetal_ped_IFALD@meta.data
+# save(cell_label, file=paste(here("../../../projects/macparland/RE/PediatricAdult/processed_data"),"Fetal_IFALD_adult_ped_cellRough.rds", sep=""))
+# 
 
-d10x_ped_IFALD<-readRDS(file = here("../../../projects/macparland/RE/PediatricAdult/processed_data","IFALD_d10x_adult_ped_raw.rds"))
-## add cell type labels
-load(here("../../../projects/macparland/RE/PediatricAdult/processed_data","IFALD_adult_ped_cellRefined_withDropletQC.rds"))
-cell_label$index<-rownames(cell_label)
-cell_label<-cell_label[match(colnames(d10x_ped_IFALD), cell_label$index),]
-identical(colnames(d10x_ped_IFALD), cell_label$index)
-d10x_ped_IFALD <- AddMetaData(d10x_ped_IFALD, metadata = cell_label)
-
-
-#################
-## Match meta data
-#################
-d10x_fetal@meta.data$Barcodes<-NULL
-colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Extract.Name")]<-"Sample"
-colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Characteristics.individual.")]<-"individual"
-d10x_fetal@meta.data$Treatment<-"Healthy"
-d10x_fetal@meta.data$Tissue<-"TLH"
-d10x_fetal@meta.data$chemistry<-"3pr"
-colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Characteristics.sex.")]<-"Sex"
-colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Characteristics.age.")]<-"Age"
-d10x_fetal@meta.data$AgeGroup<-"Fetal"
-d10x_fetal@meta.data$FreshorFrozen<-"fresh"
-d10x_fetal@meta.data$BMI<-NA
-d10x_fetal@meta.data$relALBChange<-NA
-d10x_fetal@meta.data$nuclear_fraction<-NA
-d10x_fetal@meta.data$cell_status<-NA
-d10x_fetal@meta.data$Perfused<-NA
-colnames(d10x_fetal@meta.data)[which(colnames(d10x_fetal@meta.data)=="Cell.Labels")]<-"CellType_refined"
-d10x_fetal@meta.data$age_condition<-paste(d10x_fetal$AgeGroup, d10x_fetal$Treatment, sep=" ")
-
-
-d10x_ped_IFALD@meta.data$file<-NULL
-d10x_ped_IFALD@meta.data$Approx_bam_GB<-NULL
-d10x_ped_IFALD@meta.data$Characteristics.facs.sorting.<-NA
-d10x_ped_IFALD@meta.data$Sample<-d10x_ped_IFALD@meta.data$individual
-d10x_ped_IFALD@meta.data$individual<-sapply(1:nrow(d10x_ped_IFALD@meta.data), function(x) strsplit(d10x_ped_IFALD@meta.data$individual[x],"_")[[1]][1])
-d10x_ped_IFALD@meta.data$CellType_rough <-NULL
-d10x_ped_IFALD@meta.data$second_best_cell  <-NULL
-d10x_ped_IFALD@meta.data$S.Score   <-NULL
-d10x_ped_IFALD@meta.data$G2M.Score <-NULL
-d10x_ped_IFALD@meta.data$Phase     <-NULL
-d10x_ped_IFALD@meta.data$old.ident<-NULL
-d10x_ped_IFALD@meta.data$integrated_snn_res.0.5 <-NULL
-d10x_ped_IFALD@meta.data$seurat_clusters<-NULL
-d10x_ped_IFALD@meta.data$age_id<-NULL
-d10x_ped_IFALD@meta.data$index<-NULL
-d10x_ped_IFALD@meta.data$SCINA_broad<-NULL
-d10x_ped_IFALD@meta.data$SCINA_refined<-NULL
-d10x_ped_IFALD@meta.data$cluster_consensus<-NULL
-d10x_ped_IFALD@meta.data$Characteristics.clinical.information.<-NA
-
-
-head(d10x_ped_IFALD@meta.data)
-head(d10x_fetal@meta.data)
-
-d10x_fetal@meta.data<-d10x_fetal@meta.data[,colnames(d10x_ped_IFALD@meta.data)]
-
-head(d10x_fetal@meta.data)
-head(d10x_ped_IFALD@meta.data)
-
-
-
-
-
-
-#################
-## Merge
-#################
-d10x <- merge(d10x_ped_IFALD,d10x_fetal, merge.data=TRUE, project = "IFALD_fetal_adult_ped_map")
-saveRDS(d10x, file = here("../../../projects/macparland/RE/PediatricAdult/processed_data","Fetal_IFALD_d10x_adult_ped_raw.rds"))
-
-###############
-## Integrate by donor
-###############
-#https://satijalab.org/seurat/articles/integration_rpca.html
-print("RUNNING INTEGRATION")
-
-## run integration across donor and hopefully that will also smooth out differences with chemistry and batch?
-d10x.list<- SplitObject(d10x, split.by = "Sample")
-
-# normalize, identify variable features and score cell cycle for each dataset independently
-s.genes <- cc.genes$s.genes
-g2m.genes <- cc.genes$g2m.genes
-
-d10x.list <- lapply(X = d10x.list, FUN = function(x) {
-  x <- NormalizeData(x)
-  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
-  x <- CellCycleScoring(x, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-})
-
-# select features that are repeatedly variable across datasets for integration run PCA on each
-# dataset using these features
-features <- SelectIntegrationFeatures(object.list = d10x.list)
-d10x.list <- lapply(X = d10x.list, FUN = function(x) {
-  #x <- ScaleData(x, features = features, verbose = FALSE)
-  x <- ScaleData(x, vars.to.regress = c("nFeature_RNA","S.Score", "G2M.Score"), features = features, verbose = FALSE)
-  x <- RunPCA(x, features = features, verbose = FALSE)
-})
-
-## Identify anchors
-chem.anchors <- FindIntegrationAnchors(object.list = d10x.list, anchor.features = features, reduction = "rpca")
-d10x.fetal_ped_IFALD <- IntegrateData(anchorset = chem.anchors)
-
-DefaultAssay(d10x.fetal_ped_IFALD) <- "integrated"
-
-print("INTEGRATED")
-
-
-# Run the standard workflow for visualization and clustering
-d10x.fetal_ped_IFALD <- ScaleData(d10x.fetal_ped_IFALD, verbose = FALSE)
-d10x.fetal_ped_IFALD <- RunPCA(d10x.fetal_ped_IFALD, npcs = 30, verbose = FALSE)
-d10x.fetal_ped_IFALD <- RunUMAP(d10x.fetal_ped_IFALD, reduction = "pca", dims = 1:30)
-d10x.fetal_ped_IFALD <- RunTSNE(d10x.fetal_ped_IFALD, dims = 1:30)
-
-d10x.fetal_ped_IFALD <- FindNeighbors(d10x.fetal_ped_IFALD, reduction = "pca", dims = 1:30)
-d10x.fetal_ped_IFALD <- FindClusters(d10x.fetal_ped_IFALD, resolution = 0.5)
-
-d10x.fetal_ped_IFALD
-
-
-##############
-## Save integrated to look local
-##############
-save(d10x.fetal_ped_IFALD, file=paste(here("../../../projects/macparland/RE/PediatricAdult/processed_data"),"Fetal_IFALD_adult_ped_integrated.rds", sep=""))
-cell_label<-d10x.fetal_ped_IFALD@meta.data
-save(cell_label, file=paste(here("../../../projects/macparland/RE/PediatricAdult/processed_data"),"Fetal_IFALD_adult_ped_cellRough.rds", sep=""))
-
+load(file="/cluster/projects/macparland/RE/PediatricAdult/processed_data/Fetal_IFALD_adult_ped_integrated.rds")
 
 
 
@@ -194,7 +196,6 @@ gene_exp<-melt(gene_exp)
 
 plt<-merge(gene_exp,plt, by="cell")
 
-plt<-merge(score_data,plt, by="cell")
 
 head(plt)
 
--- scripts/03_IFALD_HSC.R
+++ scripts/03_IFALD_HSC.R
@@ -50,6 +50,7 @@ d10x.combined_hsc@meta.data$CellType_rough[which(d10x.combined_hsc@meta.data$seu
 save(d10x.combined_hsc, file=here("data/HSC_integrated.RData"))
 
 
+
 table(d10x.combined_hsc@meta.data$CellType_rough, d10x.combined_hsc$age_condition)
 #Adult IFALD 70% Adult/IFLAD
 #Healthy Ped 92% healthy ped
@@ -61,6 +62,39 @@ save_plts(fancy_HSC, "IFALD_HSC_UMAP", w=4,h=3)
 
 fancy_HSC<-fanciest_UMAP(d10x.combined_hsc, NA,T)
 save_plts(fancy_HSC, "IFALD_HSC_UMAP_split", w=8,h=6)
+
+## colored by age_condition
+umap_mat_myeloid<-as.data.frame(Embeddings(object = d10x.combined_hsc, reduction = "umap"))#
+umap_mat_myeloid$cell<-rownames(umap_mat_myeloid)
+meta_myeloid<-d10x.combined_hsc@meta.data
+meta_myeloid$cell<-rownames(meta_myeloid)
+plt_myeloid<-merge(meta_myeloid, umap_mat_myeloid, by="cell")
+
+len_x_bar<-((range(plt_myeloid$UMAP_1))[2]-(range(plt_myeloid$UMAP_1))[1])/10
+len_y_bar<-((range(plt_myeloid$UMAP_2))[2]-(range(plt_myeloid$UMAP_2))[1])/10
+arr <- list(x = min(plt_myeloid$UMAP_1)-2, y = min(plt_myeloid$UMAP_2)-2, x_len = len_x_bar, y_len = len_y_bar)
+
+forlegned_plot<-ggplot(plt_myeloid, aes(UMAP_1,UMAP_2))+
+  geom_point(aes(fill=age_condition),size=2, shape=21)+xlab("UMAP 1")+ylab("UMAP 2")+
+  fillscale_agecondition+theme_bw()+
+  theme(legend.text = element_text(size=5),
+        legend.title = element_text(size=6))
+nice_legend<-get_leg(forlegned_plot)
+
+fanciest_UMAP<-ggplot(plt_myeloid, aes(UMAP_1,UMAP_2))+
+  geom_point(size = 0.06, colour= "black", stroke = 1)+
+  geom_point(aes(color=age_condition),size=0.05)+xlab("UMAP 1")+ylab("UMAP 2")+
+  colscale_agecondition+
+  annotate("segment", 
+           x = arr$x, xend = arr$x + c(arr$x_len, 0), 
+           y = arr$y, yend = arr$y + c(0, arr$y_len), size=0.25,color="black",
+           arrow = arrow(type = "closed", length = unit(2, 'pt'))) +
+  theme_void()+theme(plot.margin = margin(0.25,0.25,0.25,0.25, "cm"),
+                     axis.title.x = element_text(size=5,hjust = 0.05),
+                     axis.title.y = element_text(size=5,hjust = 0.05,angle = 90),
+                     legend.position = "none")+
+  annotate("text",x = min(plt_myeloid$UMAP_1)+(0.95*len_x_bar)-1, y = min(plt_myeloid$UMAP_2)+(0.5*len_y_bar)-2, label=paste0("n = ",comma(ncol(d10x.combined_hsc))), size=2)
+save_plts(plot_grid(fanciest_UMAP,nice_legend, rel_widths = c(5,2)), "Healthy_and_IFALD_HSC_UMAP", w=3, h=2)
 
 
 ##############
