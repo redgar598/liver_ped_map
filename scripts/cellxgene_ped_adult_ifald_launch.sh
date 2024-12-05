fa5c9a54b6fd2ca87fe1088da708e8573ab8f020
redgar598 <redgar598@gmail.com>
2024-12-05 10:04:41 -0500

    organizing to submit

--- scripts/cellxgene_ped_adult_ifald_launch.sh
+++
@@ -1,7 +0,0 @@
-conda activate cellxgene
-
-# launch on integrated data exported from R
-cellxgene launch /home/redgar/Documents/liver_ped_map/data/adult_ped_ifald.h5ad --open
-
-# launch on integrated data exported from R
-cellxgene launch /home/redgar/Documents/liver_ped_map/data/adult_ped_ifald_myeloid.h5ad --open
--- scripts/tmp.R
+++
@@ -1,6 +0,0 @@
-d10x.combined_hsc <- RunPCA(d10x.combined_hsc, npcs = 20, verbose = FALSE)
-d10x.combined_hsc <- RunUMAP(d10x.combined_hsc, reduction = "pca", dims = 1:20)
-d10x.combined_hsc <- FindNeighbors(d10x.combined_hsc, reduction = "pca", dims = 1:20)
-d10x.combined_hsc <- FindClusters(d10x.combined_hsc, resolution = 0.01)
-
-DimPlot(d10x.combined_hsc, label=T)
